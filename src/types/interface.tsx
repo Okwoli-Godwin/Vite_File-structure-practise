295319145df0325c3309b0e2b1f669509aa882eb
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-17 03:27:04 +0100

    iCard

--- src/types/interface.tsx
+++ src/types/interface.tsx
@@ -4,3 +4,41 @@ export interface iButton {
     c?: string;
     onClick?: () => void;
 }
+
+export interface iCard {
+    title?: string;
+    buttonTitle?: string;
+    onClick?: () => void;
+
+    inputTitle?: string;
+    inputTitle1?: string;
+    inputTitle2?: string;
+    request?: boolean;
+    sign?: boolean;
+
+    tex?: boolean;
+    req?: boolean;
+    textTitle?: string;
+    message?: string;
+    remake?: string;
+
+    text?: string;
+    route?: string;
+    path?: string;
+
+    call?: boolean;
+    text1?: string;
+    route1?: string;
+    path1: string;
+
+    email?: string;
+    password?: string;
+    userName?: string;
+
+    onChange?: React.ChangeEventHandler<HTMLInputElement>
+
+    setEmail?: React.Dispatch<React.SetStateAction<string>>
+    setPassword?: React.Dispatch<React.SetStateAction<string>>
+    setUserName?: React.Dispatch<React.SetStateAction<string>>
+}
+    
