75083567639306d375d4bb4f76bc08284c6464c9
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-17 08:47:39 +0100

    main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider } from "react-router-dom"
+import { mainRouter } from './router/mainRouter'
+import { ShareState } from './Global/shareState'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ShareState>
+      <RouterProvider router={mainRouter} />
+    </ShareState>
+    {/* <App /> */}
   </React.StrictMode>,
 )
