8ec6c162647780dd17909b01f0fb9276b9993c38
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-17 09:05:20 +0100

    company

---
+++ src/Hook/useCompany.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import axios from "axios"
+
+const url = "http://localhost:2026/api"
+
+export const useCompany = () => {
+    const [company, setCompany] = useState<[]>([])
+    const getAll = async () => {
+        try {
+            await axios.get(`${url}/company`).then((res) => {
+                return setCompany(res.data.data)
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getAll();
+        console.log(company)
+    }, [])
+
+    return [company] as const
+}
